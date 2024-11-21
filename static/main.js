909170835056c65ecd4b75bf81e19210824285ec
Ikumo Takahashi <ikumo1118free@gmail.com>
2024-11-21 19:48:29 +0900

    Finished section 10

--- static/main.js
+++ static/main.js
@@ -2,26 +2,21 @@ const EMPTY = 0;
 const DARK = 1;
 const RIGHT = 2;
 
-const INITIAL_BOARD = [
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, DARK, RIGHT, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, RIGHT, DARK, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-];
-
 const boardElement = document.getElementById('board');
 
-async function showBoard() {
+async function showBoard(turnCount) {
+  const response = await fetch(`/api/games/latest/turns/${turnCount}`);
+  const responseBody = await response.json();
+  const board = responseBody.board;
+  const nextDisc = responseBody.nextDisc;
+
   while (boardElement.firstChild) {
     boardElement.removeChild(boardElement.firstChild);
   }
 
-  INITIAL_BOARD.forEach((line) => {
-    line.forEach((square) => {
+  board.forEach((line, y) => {
+    line.forEach((square, x) => {
+      7;
       const squareElement = document.createElement('div');
       squareElement.className = 'square';
 
@@ -31,6 +26,12 @@ async function showBoard() {
         stoneElement.className = `stone ${color}`;
 
         squareElement.appendChild(stoneElement);
+      } else {
+        squareElement.addEventListener('click', async () => {
+          const nextTurnCount = turnCount + 1;
+          await registerTurn(nextTurnCount, nextDisc, x, y);
+          await showBoard(nextTurnCount);
+        });
       }
 
       boardElement.appendChild(squareElement);
@@ -44,9 +45,28 @@ async function registerGame() {
   });
 }
 
+async function registerTurn(turnCount, disc, x, y) {
+  const requestBody = {
+    turnCount,
+    move: {
+      disc,
+      x,
+      y,
+    },
+  };
+
+  await fetch('/api/games/latest/turns', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(requestBody),
+  });
+}
+
 async function main() {
   await registerGame();
-  await showBoard();
+  await showBoard(0);
 }
 
 main();
