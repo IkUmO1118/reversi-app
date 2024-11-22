623271c9cdaa914037c1c29b9e1dee853f940d43
Ikumo Takahashi <ikumo1118free@gmail.com>
2024-11-22 12:57:02 +0900

    Finished section 11

---
+++ src/presentation/turnRouter.ts
@@ -0,0 +1,55 @@
+import express from 'express';
+import { TurnService } from '../application/turnService';
+
+export const turnRouter = express.Router();
+
+const turnService = new TurnService();
+
+// DTO (Data Transfer Object)
+interface TurnGetResponseBody {
+  turnCount: number;
+  board: number[][];
+  nextDisc: number | null;
+  winnerDisc: number | null;
+}
+
+turnRouter.get(
+  '/api/games/latest/turns/:turnCount',
+  async (req, res: express.Response<TurnGetResponseBody>) => {
+    const turnCount = parseInt(req.params.turnCount);
+
+    const output = await turnService.findLatestGameTurByTurnCount(turnCount);
+
+    const responseBoby = {
+      turnCount: output.turnCount,
+      board: output.board,
+      nextDisc: output.nextDisc ?? null,
+      winnerDisc: output.winnerDisc ?? null,
+    };
+    res.json(responseBoby);
+  }
+);
+
+// DTO (Data Transfer Object)
+interface TurnPostRequestBody {
+  turnCount: number;
+  move: {
+    disc: number;
+    x: number;
+    y: number;
+  };
+}
+
+turnRouter.post(
+  '/api/games/latest/turns',
+  async (req: express.Request<{}, {}, TurnPostRequestBody>, res) => {
+    const turnCount = req.body.turnCount;
+    const disc = req.body.move.disc;
+    const x = req.body.move.x;
+    const y = req.body.move.y;
+
+    await turnService.registerTurn(turnCount, disc, x, y);
+
+    res.status(201).end();
+  }
+);
